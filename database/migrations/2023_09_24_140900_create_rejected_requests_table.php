207f4b98e38a31e2bc93a10af3384aabac5e435c
Dhafa <dhofaaryanda4@gmail.com>
2023-09-24 21:29:20 +0700

    add migration rejected request

---
+++ database/migrations/2023_09_24_140900_create_rejected_requests_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rejected_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaction_id')->constrained('transactions');
+            $table->String('message');
+            $table->String('refund_proof');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rejected_requests');
+    }
+};
