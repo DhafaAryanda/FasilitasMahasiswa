420d7281e160896aca554ece0be755a306809619
Dhafa <dhofaaryanda4@gmail.com>
2023-11-25 10:46:38 +0700

    create table guest_transaction

---
+++ database/migrations/2023_11_25_101707_create_guest_transactions.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('guest_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('guest_name');
+            $table->string('guest_email')->unique()->nullable();
+            $table->string('nim')->unique()->nullable();
+            $table->foreignId('facility_id')->constrained('facilities');
+            $table->String('activity_name');
+            $table->String('transaction_code');
+            $table->datetime('schedule_start');
+            $table->datetime('schedule_end');
+            $table->integer('duration_hours');
+            $table->text('description');
+            $table->String('phone_number');
+            $table->decimal('amount', 10, 2);
+            $table->String('bank_name');
+            $table->String('bank_account_number');
+            $table->String('proof_of_payment');
+            $table->String('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('guest_transactions');
+    }
+};
--- resources/views/admin/manajemen/sewa/create-transaction.blade.php
+++ resources/views/admin/manajemen/sewa/create-transaction.blade.php
@@ -1,3 +1,121 @@
 @extends('admin.layouts.base')
 
-@section('title', 'Riwayat Transaksi')
+@section('title', 'Transaksi Tanpa Akun')
+
+@section('content')
+
+@section('content')
+
+  {{-- transaction-facility --}}
+
+  <div class="p-5 lg:p-10 flex items-center justify-center font-dmsans">
+    <div class="container max-w-screen-lg mx-auto">
+      @if ($errors->any())
+        <div class="mb-4" role="alert">
+          <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+            Danger
+          </div>
+          <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+            <ul>
+              @foreach ($errors->all() as $errors)
+                <li>{{ $errors }}</li>
+              @endforeach
+            </ul>
+          </div>
+        </div>
+      @endif
+      <div>
+        <h2 class="font-semibold text-xl text-slate-700 mb-6">Sewa Fasilitas</h2>
+        <!-- Form Start -->
+        <form class="mb-20" action="#" method="POST" enctype="multipart/form-data">
+          @csrf
+          <section>
+            <div class="bg-white rounded-lg border border-gray-200 shadow-md mb-5">
+              <div class="items-center mb-3">
+                <p class=" mx-10 mt-3 mb-2  font-bold">Form Sewa Fasilitas</p>
+                <hr>
+              </div>
+              <div class="flex flex-col md:flex-row mb-5">
+
+                <div class="flex-1 mx-10 md:mx-3 lg:mx-5">
+
+                  <div class="mt-4 mb-4 md:mx-1 lg:mx-5 text-sm">
+                    <div class="sm:col-span-3 md:col-span-5 mb-5">
+                      <label for="phone_number" class="font-medium after:content-['*'] after:text-red-500 ">Nama
+                        Prasarana</label>
+                      <input type="text" name="phone_number" id="phone_number"
+                        class="h-10 border mt-2 rounded px-4 w-full bg-gray-50" />
+                      <p class="text-xs mt-2 text-[#858584]">Nama prasarana yang dipilih</p>
+                    </div>
+                    <div class="sm:col-span-3 md:col-span-5 mb-5">
+                      <label for="phone_number" class="font-medium after:content-['*'] after:text-red-500 ">Nama
+                        Penyewa</label>
+                      <input type="text" name="phone_number" id="phone_number"
+                        class="h-10 border mt-2 rounded px-4 w-full bg-gray-50" />
+                      <p class="text-xs mt-2 text-[#858584]">Nama penyewa prasarana</p>
+                    </div>
+                    <div class="sm:col-span-3 md:col-span-5 mb-5">
+                      <label for="phone_number" class="font-medium after:content-['*'] after:text-red-500 ">Nomor
+                        Telepon</label>
+                      <input type="text" name="phone_number" id="phone_number"
+                        class="h-10 border mt-2 rounded px-4 w-full bg-gray-50" />
+                      <p class="text-xs mt-2 text-[#858584]">Tuliskan Nomor Telepon yang menggunakan Whatsapp</p>
+                    </div>
+                    <div class="sm:col-span-3 md:col-span-5 mb-5">
+                      <label for="description" class="font-medium after:content-['*'] after:text-red-500 ">Deskripsi
+                        Kegiatan</label>
+                      <textarea type="text" name="description" id="description" class="h-24 border mt-2 rounded px-4 w-full bg-gray-50"
+                        placeholder=""></textarea>
+                      <p class="text-xs mt-2 text-[#858584]">Tuliskan keperluan Nomor Telepon yang menggunakan Whatsapp
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="flex-1 mx-10 md:mx-3 lg:mx-5">
+
+                  <div class="mt-4 mb-4 md:mx-1 lg:mx-5 text-sm">
+                    <div class="sm:col-span-3 md:col-span-5 mb-5">
+                      <label for="activity_name" class="font-medium after:content-['*'] after:text-red-500 ">Nama
+                        Kegiatan</label>
+                      <input type="text" name="activity_name" id="activity_name"
+                        class="h-10 border mt-2 rounded px-4 w-full bg-gray-50" />
+                      <p class="text-xs mt-2 text-[#858584]">Tuliskan nama kegiatan atau acara</p>
+                    </div>
+                    <div class="sm:col-span-3 md:col-span-5 mb-5">
+                      <label for="dateandtimepicker1" class="font-medium after:content-['*'] after:text-red-500 ">Jadwal
+                        Kegiatan Berlangsung</label>
+                      <input id="dateandtimepicker1" type="text" name="schedule_start" id="schedule_start"
+                        class="h-10 border mt-2 rounded px-4 w-full bg-gray-50" />
+                      <p class="text-xs mt-2 text-[#858584]">Pilih jadwal kegiatan berlangsung</p>
+                    </div>
+                    <div class="sm:col-span-3 md:col-span-5 mb-5">
+                      <label for="duration_hours" class="font-medium after:content-['*'] after:text-red-500 ">Durasi
+                        Kegiatan</label>
+                      <input type="text" name="duration_hours" id="duration_hours"
+                        class="h-10 border mt-2 rounded px-4 w-full bg-gray-50" />
+                      <p class="text-xs mt-2 text-[#858584]">Tuliskan durasi kegiatan dalam jam</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+
+          <div class="flex justify-end mx-10">
+            <a href="{{ route('member.dashboard') }}"
+              class="bg-gray-400 mx-2 hover:bg-gray-600 text-white font-semibold py-2 px-4 rounded-lg">Kembali</a>
+            <button type="submit" name="action" value="update"
+              class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg">Berikutnya</button>
+          </div>
+
+        </form>
+
+      </div>
+    </div>
+  </div>
+
+
+@endsection
+
+@endsection
